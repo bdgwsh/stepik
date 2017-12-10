45c6b5d75d9190b596fd96a709184db99045aa58
Sergey Konik <bdwsh@gmail.com>
2017-12-10 08:10:04 +0000

    fixed conf and init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c "etc/hello.py" hello:app
+gunicorn -c "etc/hello.py" hello:app &
+cd ask
+gunicorn -c "etc/django-gunicorn.py" ask.wsgi:application
