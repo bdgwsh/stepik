dccbdb5d8bfeb463d39f7b7f8b88559193b139b7
Sergey Konik <bdgwsh@gmail.com>
2017-12-10 18:43:07 +0300

    ..

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
@@ -16,11 +24,3 @@ class Answer(models.Model):
     addet_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User)
-
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
